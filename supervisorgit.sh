b6f681f148958eb958e3f4122873a742c75848b9
Avadhesh Mishra <avadhesh.mishra@gmail.com>
2018-03-31 01:01:05 +0530

    commited by ubuntu sync

--- supervisorgit.sh
+++ supervisorgit.sh
@@ -1,6 +1,4 @@
 cd /var/www/html
-git config user.email "avadhesh.mishra@gmail.com"
-git config user.name "Avadhesh Mishra"
 git pull origin master
 git add .
 git commit -m 'commited by ubuntu sync'
