d7807befee8369f05cd655917c499cc9e7a491f5
Avadhesh Mishra <avadhesh.mishra@gmail.com>
2018-03-31 01:25:42 +0530

    commited by ubuntu sync

--- grive.sh
+++ grive.sh
@@ -1,5 +1,3 @@
-cd /var/www/html
-git pull origin master
-git add .
-git commit -m 'commited by ubuntu sync'
-git push https://avmishra:github.com/avmishra/html.git master
+cd /home/avadhesh/Documents/pepperfry
+sleep 6
+grive
