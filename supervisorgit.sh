75c2e2a5668c77ac1de03d19b81558a16de7d672
Avadhesh Mishra <avadhesh.mishra@gmail.com>
2018-03-30 16:56:47 +0530

    commited by ubuntu sync

--- supervisorgit.sh
+++ supervisorgit.sh
@@ -1,5 +1,4 @@
 cd /var/www/html
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git config user.email "avadhesh.mishra@gmail.com"
 git config user.name "Avadhesh Mishra"
 git pull origin master
